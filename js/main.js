d057bc520a64df46be6061b8174fb1a6c5caa9d0
Anurag Phukan <reach.anuragph@gmail.com>
2021-01-20 11:50:38 +0530

    Add GUI

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Make the computer's selection
 function computerPlay() {
     let chooseFrom = ['rock', 'paper', 'scissors'];
     let computerSelection = chooseFrom[Math.floor(Math.random() * 3)];
@@ -5,78 +6,109 @@ function computerPlay() {
     return computerSelection;
 }
 
-function playRound(playerSelection, computerSelection) {
+// Make the player's selection from click event and play one round
+function playRound(playerSelection) {
 
-    playerSelection  = prompt("Choose 'Rock', 'Paper' or 'Scissors': ");
+    playerSelection  = playerSelection.target.value; // triggered by click event
     playerSelection  = playerSelection .toLowerCase();
 
     computerSelection = computerPlay();
     
     let message = '';
     
+    // Game logic
     if (playerSelection == 'rock') {
         switch(computerSelection) {
             case 'rock':
                 message = "It's a draw.";
                 break;
             case 'paper':
-                message = 'Paper beats rock. You lose.';
-                score--;
+                message = 'Paper beats rock.';
+                computerScore++;
                 break;
             case 'scissors':
-                message = 'Rock beats scissors. You win!';
-                score++;
+                message = 'Rock beats scissors.';
+                playerScore++;
         }  
     } else if(playerSelection == 'paper') {
         switch(computerSelection) {
             case 'rock':
-                message = 'Paper beats rock. You win!';
-                score++;
+                message = 'Paper beats rock.';
+                playerScore++;
                 break;
             case 'paper':
                 message = "It's a draw";
                 break;
             case 'scissors':
-                message = 'Scissors beat paper. You lose.';
-                score--;
+                message = 'Scissors beat paper.';
+                computerScore++;
         }
     } else {
         switch(computerSelection) {
             case 'rock':
-                message = 'Rock beats scissors. You lose.';
-                score--;
+                message = 'Rock beats scissors.';
+                computerScore++;
                 break;
             case 'paper':
-                message = 'Scissors beat paper. You win!';
-                score++
+                message = 'Scissors beat paper.';
+                playerScore++;
                 break;
             case 'scissors':
                 message = "It's a draw";
         }   
     }
-    message = `You chose ${playerSelection}.\nComputer chose ${computerSelection}.\n${message}`
-    console.log(message);
-    return score;
+    message = `You chose ${playerSelection}. Computer chose ${computerSelection}. ${message}`;
+    scoreArray = [playerScore, computerScore];
+    roundResults(message, scoreArray);
+    return scoreArray;
 }
 
-function game() {
+
+// Show results and live score in DOM
+function roundResults(message, scoreArray) {
+
+    playerScore = scoreArray[0];
+    computerScore = scoreArray[1];
+
+    const resultText = document.querySelector('#result-text');
+    const scoreboard = document.querySelector('#scoreboard');
     
-    for(let i=0; i<5; i++) {
-        playRound(playerSelection , computerSelection);
-    }
+    resultText.textContent = message;
+    scoreboard.textContent = `You: ${playerScore} | Computer: ${computerScore}`;
+}
 
-    if (score > 0) {
-        console.log('YOU WoN THE GAME!! :)');
-    } else if (score < 0) {
-        console.log('YOU LOST THE GAME :(');
+
+// Declare final winner
+function declareWinner() {
+
+    const winnerText = document.querySelector('#winner-text');
+    if (playerScore === 5) {
+        winnerText.innerHTML = '<p>YOU WIN! :) <br> ...Refresh page to play again.</p>'
     } else {
-        console.log('THE GAME ENDED IN A DRAW');
+        winnerText.innerHTML = '<p>The computer wins. :( <br> ...Refresh page to play again.</p>'
     }
 }
 
-let score = 0;
+
+let scoreArray = [0, 0];
+let playerScore = scoreArray[0];
+let computerScore = scoreArray[1];
 
 let playerSelection;
 let computerSelection;
 
-game();
+const gameButtons = document.querySelectorAll('.game-button');
+
+// Create event listeners for each button to call playRound(..)
+// and declare winner
+gameButtons.forEach((gameButton) => {
+    gameButton.addEventListener('click', (e) => {
+        playRound(e);
+        console.log(playerScore);
+        console.log(computerScore);
+        if (playerScore === 5 || computerScore === 5) {
+            declareWinner();
+        }
+    });
+})
+
